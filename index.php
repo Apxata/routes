8ab0ed8bdb04a2d37878ffed8815589522f6bd81
apxat <apxata@gmail.com>
2018-09-20 20:09:01 +0300

    first routes commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$url = substr($_SERVER['REQUEST_URI'], 1);
+$url = rtrim($url, '/');
+$url = explode('/', $url);
+
+require 'controllers/' . $url[0] . '.php';
+$controller = new $url[0];
+if (isset($url[2])) {
+    $controller->$url[1]($url[2]);
+} else {
+    if (isset($url[1])) {
+        $controller->$url[1]();
+    }
+}
